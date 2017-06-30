3728e07e6216df3b90067981939a209279242fd3
Gabor Csikos <csiki123@gmail.com>
2017-06-30 19:07:06 +0200

    first commit

---
+++ JPMorgan/src/gabor/csikos/main/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package gabor.csikos.main;
+
+/**
+ * @author Gabor Csikos
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
